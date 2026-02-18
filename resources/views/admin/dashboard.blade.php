0c192f8b407eebb4b23ddcade259a7971b3c526e
unluciox <unluciox112@hotmail.com>
2026-02-18 10:42:33 -0600

    feat(admin): add admin layout with Flowbite sidebar/navigation

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,3 @@
+<x-admin-layout>
+  Hola desde el panel de control administrativo.
+</x-admin-layout>
--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function(){
-  return 'AwaS';
-});
+  return view('admin.dashboard');
+})->name('dashboard');
 
