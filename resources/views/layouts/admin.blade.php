695709e080421dce4dbd1c6ab0afd13968eeb68f
unluciox <unluciox112@hotmail.com>
2026-02-26 19:58:40 -0600

    chore: install spatie/laravel-permission, datatables and wireui

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -19,6 +19,8 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <script src="https://kit.fontawesome.com/b2bb8bbf2e.js" crossorigin="anonymous"></script>
+            <!-- WireUI -->
+            <wireui:scripts />
 
         <!-- Styles -->
         @livewireStyles
